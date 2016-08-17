23e7db8fc88e467cb52f0052071d323d50e1f3e9
Hugh David <maple10001@foxmail.com>
2016-08-17 11:50:40 -0700

    create save course thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,12 +5,31 @@ export function loadCoursesSuceess(courses) {
   return {type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
+export function createCourseSuccess(courses) {
+  return {type: types.CREATE_COURSES_SUCCESS, courses};
+}
+
+export function updateCourseSuccess(courses) {
+  return {type: types.UPDATE_COURSES_SUCCESS, courses};
+}
+
 export function loadCourses () {
   return function (dispatch) {
     return courseApi.getAllCourses().then(courses => {
         dispatch(loadCoursesSuceess(courses));
     }).catch( error => {
       throw(error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return (dispatch, getState) => {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+      dispatch(createCourseSuccess(savedCourse));
+    }).catch(error => {
+      throw(error)
     });
   };
 }
